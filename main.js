fd7c91cd86ec1e15ff97fba3019b37b334f538da
Andrey Kernichniy <yoyo100295@hotmail.com>
2016-11-28 23:39:29 +0600

    change listen port

--- main.js
+++ main.js
@@ -8,6 +8,6 @@ app.get('/', function(req,res){
 
 app.use(express.static(__dirname + '/static'));
 
-app.listen(6024, function(){
-    console.log('Listen port 6024');
+app.listen(6025, function(){
+    console.log('Listen port 6025');
 })
